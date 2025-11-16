6424961e84603e931810d21df286be31c471a0ab
khiem <khiemnguyen.hye@gmail.com>
2025-11-16 23:52:36 +0700

    Implement change detection and conditional commit.

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
 	runCmd("git", "add", ".")
 
-	commitMsg := "Auto generated: Made changes to the code."
-	runCmd("git", "commit", "-m", commitMsg)
+	filesChanged := getChanges()
+	if len(filesChanged) == 0 {
+		fmt.Println("No file change. No commit made.")
+		return
+	}
+
+    fmt.Println(filesChanged)
+
+	// commitMsg := "Auto generated: Made changes to the code."
+	// runCmd("git", "commit", "-m", commitMsg)
 }
 
 func runCmd(name string, args ...string) {
@@ -17,5 +27,20 @@ func runCmd(name string, args ...string) {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
-	cmd.Run();
+	cmd.Run()
+}
+
+func getChanges() []string {
+	cmd := exec.Command("git", "diff", "--cached", "--name-only")
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error getting changes:", err);
+		return nil
+	}
+
+	if strings.TrimSpace(string(out)) == "" {
+		return []string{}
+	}
+
+	return strings.Split(strings.TrimSpace(string(out)), "\n")
 }
