207305f83a710076391eba18b1cf4220e478e878
khiem <khiemnguyen.hye@gmail.com>
2025-11-16 22:19:49 +0700

    Execute  and print output.

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
 	fmt.Println("Hello world!")
 
-	for i := range 10 {
-		if i % 2 == 0 {
-			fmt.Println("The index we are at is divisible by 2")
-		} else {
-			fmt.Println("The index we are at not divisible by 2")
-		}
+	command := exec.Command("git", "--no-pager" ,"diff")
+
+	output, err := command.Output()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	fmt.Println(string(output))
 }
